b8db50b49fdd73787172d3cc7ba53b39e2141b01
Cansu uğur <99813607+cansu05@users.noreply.github.com>
2025-07-03 16:35:58 +0300

    Set up project structure and add core components and styles
    - Added assets (images, CSS wrappers)
    - Added new page components (Landing, Dashboard, Register, Error)
    - Added shared components (Logo, FormRow)
    - Removed default CRA files (App.css, tests)
    - Updated index.html and favicon

---
+++ src/pages/Register.js
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import Wrapper from "../assets/wrappers/RegisterPage";
+import { FormRow, Logo } from "../components";
+import { toast } from "react-toastify";
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
+
+const Register = () => {
+  const [values, setValues] = useState(initialState);
+
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, password, isMember } = values;
+    if (!email || !password || (!isMember && !name)) {
+      toast.error("Please Fill Out All Fields");
+      return;
+    }
+  };
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setValues({ ...values, [name]: value });
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
+        {!values.isMember && (
+          <FormRow
+            type="text"
+            name="name"
+            handleChange={handleChange}
+            value={values.name}
+          />
+        )}
+
+        <FormRow
+          type="email"
+          name="email"
+          handleChange={handleChange}
+          value={values.email}
+        />
+        <FormRow
+          type="password"
+          name="password"
+          handleChange={handleChange}
+          value={values.password}
+        />
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <p>
+          {values.isMember ? "Not a member yet?" : "Already a member?"}
+          <button type="button" className="member-btn" onClick={toggleMember}>
+            {values.isMember ? "Register" : "Login"}
+          </button>
+        </p>
+      </form>
+    </Wrapper>
+  );
+};
+export default Register;
